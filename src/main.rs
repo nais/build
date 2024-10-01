02a4ba8a54a52ebb47610fee9074714f63caaef8
Kyrre Havik <kyrremann@gmail.com>
2024-10-01 10:29:36 +0200

    feat: lagt til clap for cli commmands
    Co-authored-by: Kim Tore Jensen <kimtjen@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Check if the environment is ready
+    Check {
+        /// The environment to check
+        #[arg(short, long, default_value = "development")]
+        environment: String,
+    },
+    /// Build the Dockerfile
+    Dockerfile,
+    /// Build builds
+    Build,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Check { environment } => println!("hello {}", environment),
+        Commands::Dockerfile => {}
+        Commands::Build => {}
+    }
 }

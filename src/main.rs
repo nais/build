11e34afb5de3cee23da5d9c28666f46e35b3bd1c
Kyrre Havik <kyrremann@gmail.com>
2024-10-09 15:01:54 +0200

    feat: refaktorert Goland ut i egen SDK modul
    Co-authored-by: Kim Tore Jensen <kimtjen@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use clap::{Parser, Subcommand};
 use std::io::Write;
 use std::process::{ExitStatus, Stdio};
 use thiserror::Error;
+use sdk::DockerFileBuilder;
 
 #[allow(dead_code)]
 mod config;
 mod nais_yaml;
+mod sdk;
 
 /// NAISly build, test, lint, check and deploy your application.
 #[derive(Parser, Debug)]
@@ -35,24 +37,9 @@ enum Commands {
 }
 
 #[derive(Error, Debug)]
-pub enum DetectBuildTargetError {
-    #[error("filesystem error: {0}")]
-    FilesystemError(#[from] std::io::Error),
-
-    #[error("can't find {1}: {0} ")]
-    FileError(std::io::Error, String),
-
-    #[error("target name is empty")]
-    EmptyFilename,
-}
-
-#[derive(Error, Debug)]
 pub enum Error {
     #[error("no compatible SDKs for this source directory")]
     SDKNotDetected,
-
-    #[error("detect build target: {0}")]
-    DetectBuildTargetError(#[from] DetectBuildTargetError),
 
     #[error("filesystem error: {0}")]
     FilesystemError(#[from] std::io::Error),
@@ -68,6 +55,10 @@ pub enum Error {
 
     #[error("detect nais.yaml: {0}")]
     DetectNaisYaml(#[from] nais_yaml::Error),
+
+
+    #[error("build error: {0}")]
+    SDKError(#[from] sdk::Error),
 }
 
 /// Read configuration file from disk.
@@ -148,16 +139,6 @@ fn main() -> Result<(), Error> {
     }
 }
 
-/// SDK is anything that can produce artifacts
-trait DockerFileBuilder {
-    fn builder_docker_image(&self) -> String;
-    fn runtime_docker_image(&self) -> String;
-    fn detect_build_targets(&self) -> Result<Vec<String>, DetectBuildTargetError>;
-    fn dockerfile(&self) -> Result<String, Error>;
-    fn filesystem_path(&self) -> String;
-    //fn docker_image_name_tagged(&self) -> String;
-}
-
 fn build(docker_file_builder: Box<dyn DockerFileBuilder>, tag: &str) -> Result<(), Error> {
     let mut file = tempfile::NamedTempFile::new()?;
     file.write_all(docker_file_builder.dockerfile()?.as_bytes())?;
@@ -180,11 +161,12 @@ fn build(docker_file_builder: Box<dyn DockerFileBuilder>, tag: &str) -> Result<(
             }
         })?
 }
+
 fn init_sdk(
     filesystem_path: &str,
     cfg: &config::file::File,
 ) -> Result<Box<dyn DockerFileBuilder>, Error> {
-    match golang::new(golang::Config {
+    match sdk::golang::new(sdk::golang::Config {
         filesystem_path: filesystem_path.to_string(),
         docker_builder_image: cfg.sdk.go.build_docker_image.clone(),
         docker_runtime_image: cfg.sdk.go.runtime_docker_image.clone(),
@@ -195,142 +177,10 @@ fn init_sdk(
             return Ok(Box::new(sdk));
         }
         Ok(None) => {}
-        Err(err) => return Err(err),
+        Err(err) => return Err(Error::from(err)),
     }
 
     // try next sdk
 
     Err(SDKNotDetected)
-}
-
-/// Build Go projects.
-pub mod golang {
-    use crate::DetectBuildTargetError::EmptyFilename;
-    use crate::{DetectBuildTargetError, DockerFileBuilder, Error};
-
-    pub struct Golang(Config);
-
-    pub struct Config {
-        pub filesystem_path: String,
-        pub docker_builder_image: String,
-        pub docker_runtime_image: String,
-
-        #[allow(dead_code)]
-        pub start_hook: Option<String>,
-        #[allow(dead_code)]
-        pub end_hook: Option<String>,
-    }
-
-    pub fn new(cfg: Config) -> Result<Option<Golang>, Error> {
-        let Ok(file_stat) = std::fs::metadata(cfg.filesystem_path.to_owned() + "/go.mod") else {
-            return Ok(None);
-        };
-        if !file_stat.is_file() {
-            return Ok(None);
-        }
-
-        Ok(Some(Golang(cfg)))
-    }
-
-    impl DockerFileBuilder for Golang {
-        fn builder_docker_image(&self) -> String {
-            self.0.docker_builder_image.clone()
-        }
-
-        fn runtime_docker_image(&self) -> String {
-            self.0.docker_runtime_image.clone()
-        }
-
-        /// Return a list of binaries that can be built.
-        fn detect_build_targets(&self) -> Result<Vec<String>, DetectBuildTargetError> {
-            let targets_path = format!("{}/cmd", self.0.filesystem_path.to_owned());
-            std::fs::read_dir(&targets_path)
-                .map_err(|e| DetectBuildTargetError::FileError(e, targets_path.clone()))?
-                .map(|dir_entry| {
-                    Ok(dir_entry?
-                        .file_name()
-                        .to_str()
-                        .ok_or(EmptyFilename)?
-                        .to_string())
-                })
-                .collect()
-        }
-
-        fn dockerfile(&self) -> Result<String, Error> {
-            let targets = self.detect_build_targets()?;
-            let builder_image = &self.builder_docker_image();
-            let runtime_image = &self.runtime_docker_image();
-            let binary_build_commands: String = targets
-                .iter()
-                .map(|item| {
-                    format!(
-                        "RUN go build -a -installsuffix cgo -o /build/{} ./cmd/{}",
-                        item, item
-                    )
-                })
-                .fold(String::new(), |acc, item| acc + "\n" + &item)
-                .trim()
-                .to_string();
-            let binary_copy_commands: String = targets
-                .iter()
-                .map(|item| format!("COPY --from=builder /build/{} /app/{}", item, item))
-                .fold(String::new(), |acc, item| acc + "\n" + &item)
-                .trim()
-                .to_string();
-            let default_target = if targets.len() == 1 {
-                format!(r#"CMD ["/app/{}"]"#, targets[0])
-            } else {
-                "# Default CMD omitted due to multiple targets specified".to_string()
-            };
-
-            Ok(format!(
-                r#"
-# Dockerfile generated by NAIS build (version) at (timestamp)
-
-#
-# Builder image
-#
-FROM {builder_image} AS builder
-ENV GOOS=linux
-ENV CGO_ENABLED=0
-WORKDIR /src
-
-# Copy go.mod and go.sum files into source directory
-# so that dependencies can be downloaded before the source code.
-# This is a cache optimization step (???)
-COPY go.* /src/
-RUN go mod download
-COPY . /src
-
-# Start hook is run before testing
-#RUN ___start_hook
-
-# Test all modules
-RUN go test ./...
-
-# Build all binaries found in ./cmd/*
-{binary_build_commands}
-
-# End hook is run after build
-#RUN ___end_hook
-
-#
-# Runtime image
-#
-FROM {runtime_image}
-WORKDIR /app
-{binary_copy_commands}
-{default_target}
-"#,
-            ))
-        }
-
-        fn filesystem_path(&self) -> String {
-            self.0.filesystem_path.clone()
-        }
-
-        // fn docker_image_name_tagged(&self) -> String {
-        //     self.0.docker_image_name_tagged.clone()
-        // }
-    }
 }

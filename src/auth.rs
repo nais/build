024f8f2ae468f128548e1496ef355f70d5d4892f
Kim Tore Jensen <kimtjen@gmail.com>
2024-12-06 15:07:59 +0100

    feat: re-implement service account name generation from api-reconcilers
    Co-authored-by: Trong Huu Nguyen <trong.huu.nguyen@nav.no>

--- src/auth.rs
+++ src/auth.rs
@@ -15,7 +15,7 @@ pub enum Error {
     Reqwest(#[from] reqwest::Error),
 
     #[error("code: {0}, body: {1}")] // FIXME: REMOVE THIS YOU DUMB DUMBS
-    Deserialize(u16, String)
+    Deserialize(u16, String),
 }
 
 pub async fn token() -> Result<String, Error> {
@@ -128,3 +128,83 @@ pub async fn github_id_token(url: &str, bearer_token: &str, workload_identity_po
         }
     }
 }
+
+#[cfg(test)]
+#[test]
+fn test_gar_service_account_id() {
+    let slug = "crm-arbeidsforhold-admin";
+    let id = gar_service_account_id(slug, "nais-management-233d");
+    let expected = "projects/nais-management-233d/serviceAccounts/gar-crm-arbeidsforhold-ad-4789@nais-management-233d.iam.gserviceaccount.com";
+    assert_eq!(id, expected);
+}
+
+/// Derive the Google service account name for GAR deployment, given a application slug and a Google cloud project ID.
+///
+/// Note: this is a re-implementation of `serviceAccountNameAndAccountID` from the api-reconcilers project.
+fn gar_service_account_id(slug: &str, project_id: &str) -> String {
+    const GAR_SERVICE_ACCOUNT_PREFIX: &'static str = "gar";
+    const GOOGLE_SERVICE_ACCOUNT_MAX_LENGTH: usize = 30;
+
+    let account_id = slug_hash_prefix_truncate(
+        slug,
+        GAR_SERVICE_ACCOUNT_PREFIX,
+        GOOGLE_SERVICE_ACCOUNT_MAX_LENGTH
+    ).unwrap(); // unwrap ok due to correct MAX_LENGTH, this will never fail[tm]
+    let email_address = format!("{account_id}@{project_id}.iam.gserviceaccount.com");
+    format!("projects/{project_id}/serviceAccounts/{email_address}")
+}
+
+/// Concatenate slug and prefix into `<PREFIX>-<TRUNCATED_SLUG>-<HASH>`, where:
+/// * `PREFIX` is left as-is,
+/// * `TRUNCATED_SLUG` is the part of the slug that still fits into the string after everything is assembled to the maximum length, and
+/// * `HASH` is the first four characters of the hex-encoded SHA256 sum of the slug.
+///
+/// `max_length` must be at least `prefix_len` + 6, otherwise the length of the truncated slug
+/// would end up below zero. In this case, `None` is returned.
+///
+/// Note: this is a re-implementation of `SlugHashPrefixTruncate` from the api-reconcilers project.
+fn slug_hash_prefix_truncate(slug: &str, prefix: &str, max_length: usize) -> Option<String> {
+    const HASH_LENGTH: usize = 4;
+    let hashed_slug = sha256::digest(slug);
+    let prefix_len = prefix.len() as isize;
+    let slug_length = max_length as isize - prefix_len - HASH_LENGTH as isize - 2;
+    if slug_length < 0 {
+        return None;
+    }
+    let trimmed = truncate(slug, slug_length.max(0) as usize);
+    let truncated = truncate(&hashed_slug, HASH_LENGTH.max(0));
+    Some(vec![prefix, trimmed, truncated].join("-").to_string())
+}
+
+#[cfg(test)]
+#[test]
+fn test_slug_hash_prefix_truncate() {
+    const MAX_LENGTH: usize = 30;
+    let slug = "crm-arbeidsforhold-admin";
+    let prefix="gar";
+    let expected = "gar-crm-arbeidsforhold-ad-4789";
+    let result = slug_hash_prefix_truncate(slug, prefix, MAX_LENGTH).unwrap();
+    assert_eq!(result, expected);
+    assert_eq!(result.len(), MAX_LENGTH);
+}
+
+#[cfg(test)]
+#[test]
+fn test_slug_hash_prefix_truncate_out_of_bounds() {
+    const MAX_LENGTH: usize = 9;
+    let slug = "very-long-slug-that-must-be-truncated";
+    let prefix="four";
+    let result = slug_hash_prefix_truncate(slug, prefix, MAX_LENGTH);
+    assert_eq!(result, None);
+}
+
+
+/// Helper function for truncating a string without panicking.
+fn truncate(s: &str, length: usize) -> &str {
+    if s.len() < length {
+        s
+    } else {
+        &s[0..length]
+    }
+}
+
